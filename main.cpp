9ac98c91617d27f3bcd248ed5e8d27f398bc7684
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2012-04-29 23:17:51 +0300

    added texture binding template

--- main.cpp
+++ main.cpp
@@ -1,17 +1,172 @@
+/*
+ *		This Code Was Created By Jeff Molofee 2000
+ *		A HUGE Thanks To Fredric Echols For Cleaning Up
+ *		And Optimizing The Base Code, Making It More Flexible!
+ *		If You've Found This Code Useful, Please Let Me Know.
+ *		Visit My Site At nehe.gamedev.net
+ */
+
 #include <SFML/Graphics.hpp>
+
+#define FALSE false
+#define TRUE true
+
+bool	fullscreen=FALSE;	// Fullscreen Flag Set To Fullscreen Mode By Default
+bool    vsync=TRUE;         // Turn VSYNC on/off
+bool	light;				// Lighting ON/OFF ( NEW )
+
+GLfloat	xrot;				// X Rotation
+GLfloat	yrot;				// Y Rotation
+GLfloat xspeed;				// X Rotation Speed
+GLfloat yspeed;				// Y Rotation Speed
+GLfloat	z=-5.0f;			// Depth Into The Screen
+
+GLfloat LightAmbient[]=		{ 0.5f, 0.5f, 0.5f, 1.0f };
+GLfloat LightDiffuse[]=		{ 1.0f, 1.0f, 1.0f, 1.0f };
+GLfloat LightPosition[]=	{ 0.0f, 0.0f, 2.0f, 1.0f };
+
+GLuint	filter;				// Which Filter To Use
+GLuint	texture[3];			// Storage For 3 Textures
+
+int LoadGLTextures()									// Load Bitmaps And Convert To Textures
+{
+	int Status=FALSE;									// Status Indicator
+
+	// Load The Bitmap, Check For Errors, If Bitmap's Not Found Quit
+	sf::Image Image;
+	if (Image.LoadFromFile("Data/NeHe.bmp"))
+	{
+		Status=TRUE;									// Set The Status To TRUE
+
+        glGenTextures(3, &texture[0]);					// Create Three Textures
+
+		// Create Nearest Filtered Texture
+		glBindTexture(GL_TEXTURE_2D, texture[0]);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+		glTexImage2D(GL_TEXTURE_2D, 0, 3, Image.GetWidth(), Image.GetHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, Image.GetPixelsPtr());
+
+		// Create Linear Filtered Texture
+		glBindTexture(GL_TEXTURE_2D, texture[1]);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+		glTexImage2D(GL_TEXTURE_2D, 0, 3, Image.GetWidth(), Image.GetHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, Image.GetPixelsPtr());
+
+		// Create MipMapped Texture
+		glBindTexture(GL_TEXTURE_2D, texture[2]);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_NEAREST);
+		gluBuild2DMipmaps(GL_TEXTURE_2D, 3, Image.GetWidth(), Image.GetHeight(), GL_RGBA, GL_UNSIGNED_BYTE, Image.GetPixelsPtr());
+	}
+
+	return Status;										// Return The Status
+}
+
+GLvoid ReSizeGLScene(GLsizei width, GLsizei height)		// Resize And Initialize The GL Window
+{
+	if (height==0)										// Prevent A Divide By Zero By
+	{
+		height=1;										// Making Height Equal One
+	}
+
+	glViewport(0,0,width,height);						// Reset The Current Viewport
+
+	glMatrixMode(GL_PROJECTION);						// Select The Projection Matrix
+	glLoadIdentity();									// Reset The Projection Matrix
+
+	// Calculate The Aspect Ratio Of The Window
+	gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,100.0f);
+
+	glMatrixMode(GL_MODELVIEW);							// Select The Modelview Matrix
+	glLoadIdentity();									// Reset The Modelview Matrix
+}
+
+int InitGL()										    // All Setup For OpenGL Goes Here
+{
+    if (!LoadGLTextures())								// Jump To Texture Loading Routine
+	{
+		return FALSE;									// If Texture Didn't Load Return FALSE
+	}
+
+	glEnable(GL_TEXTURE_2D);							// Enable Texture Mapping
+	glShadeModel(GL_SMOOTH);							// Enable Smooth Shading
+	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);				// Black Background
+	glClearDepth(1.0f);									// Depth Buffer Setup
+	glEnable(GL_DEPTH_TEST);							// Enables Depth Testing
+	glDepthFunc(GL_LEQUAL);								// The Type Of Depth Testing To Do
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	// Really Nice Perspective Calculations
+
+    glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient);		// Setup The Ambient Light
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse);		// Setup The Diffuse Light
+	glLightfv(GL_LIGHT1, GL_POSITION,LightPosition);	// Position The Light
+	glEnable(GL_LIGHT1);								// Enable Light One
+	return TRUE;										// Initialization Went OK
+}
+
+int DrawGLScene()									    // Here's Where We Do All The Drawing
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear The Screen And The Depth Buffer
+	glLoadIdentity();									// Reset The View
+	glTranslatef(0.0f,0.0f,z);
+
+	glRotatef(xrot,1.0f,0.0f,0.0f);
+	glRotatef(yrot,0.0f,1.0f,0.0f);
+
+	glBindTexture(GL_TEXTURE_2D, texture[filter]);
+
+	glBegin(GL_QUADS);
+		// Front Face
+		glNormal3f( 0.0f, 0.0f, 1.0f);
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+		glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
+		// Back Face
+		glNormal3f( 0.0f, 0.0f,-1.0f);
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+		glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+		glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+		// Top Face
+		glNormal3f( 0.0f, 1.0f, 0.0f);
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
+		glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+		// Bottom Face
+		glNormal3f( 0.0f,-1.0f, 0.0f);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+		glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+		glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+		// Right face
+		glNormal3f( 1.0f, 0.0f, 0.0f);
+		glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+		glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
+		glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+		// Left Face
+		glNormal3f(-1.0f, 0.0f, 0.0f);
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+	glEnd();
+
+	xrot+=xspeed;
+	yrot+=yspeed;
+	return TRUE;										// Keep Going
+}
 
 int main()
 {
     // Create the main window
-    sf::RenderWindow App(sf::VideoMode(800, 600), "SFML window");
+    sf::Window App(sf::VideoMode(800, 600, 32), "SFML/NeHe OpenGL");
 
-    // Load a sprite to display
-    sf::Image Image;
-    if (!Image.LoadFromFile("cb.bmp"))
-        return EXIT_FAILURE;
-    sf::Sprite Sprite(Image);
+    InitGL();
+    ReSizeGLScene(800, 600);
 
-	// Start the game loop
+    // Start game loop
     while (App.IsOpened())
     {
         // Process events
@@ -21,15 +176,80 @@ int main()
             // Close window : exit
             if (Event.Type == sf::Event::Closed)
                 App.Close();
+
+            // Resize event : adjust viewport
+            if (Event.Type == sf::Event::Resized)
+                ReSizeGLScene(Event.Size.Width, Event.Size.Height);
+
+            // Handle Keyboard Events
+            if (Event.Type == sf::Event::KeyPressed) {
+                switch (Event.Key.Code) {
+                    case sf::Key::Escape:
+                        App.Close();
+                        break;
+                    case sf::Key::F1:
+                        fullscreen = !fullscreen;
+                        App.Create(fullscreen ? sf::VideoMode::GetDesktopMode() : sf::VideoMode(800, 600, 32) , "SFML/NeHe OpenGL",
+                        (fullscreen ? sf::Style::Fullscreen : sf::Style::Resize | sf::Style::Close));
+                        ReSizeGLScene(App.GetWidth(),App.GetHeight());
+                        break;
+                    case sf::Key::F5:
+                        vsync = !vsync;
+                        break;
+                    case sf::Key::L:
+                        light=!light;
+                        if (!light) {
+                            glDisable(GL_LIGHTING);
+                        } else {
+                            glEnable(GL_LIGHTING);
+                        }
+                        break;
+                    case sf::Key::F:
+                        filter+=1;
+                        if (filter>2) {
+                            filter=0;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
 
-        // Clear screen
-        App.Clear();
+        //Handle movement keys
+        const sf::Input& Input = App.GetInput();
 
-        // Draw the sprite
-        App.Draw(Sprite);
+        if (Input.IsKeyDown(sf::Key::PageUp)) {
+            z-=0.02f;
+        }
+        if (Input.IsKeyDown(sf::Key::PageDown)) {
+            z+=0.02f;
+        }
+        if (Input.IsKeyDown(sf::Key::Up)) {
+            xspeed-=0.01f;
+        }
+        if (Input.IsKeyDown(sf::Key::Down)) {
+            xspeed+=0.01f;
+        }
+        if (Input.IsKeyDown(sf::Key::Right)) {
+            yspeed+=0.01f;
+        }
+        if (Input.IsKeyDown(sf::Key::Left)) {
+            yspeed-=0.01f;
+        }
 
-        // Update the window
+        // Turn VSYNC on so that animations run at a more reasonable speed on new CPU's/GPU's.
+        App.UseVerticalSync(vsync);
+
+        // Set the active window before using OpenGL commands
+        // It's useless here because active window is always the same,
+        // but don't forget it if you use multiple windows or controls
+        App.SetActive();
+
+        //Draw some pretty stuff
+        DrawGLScene();
+
+        // Finally, display rendered frame on screen
         App.Display();
     }
 
