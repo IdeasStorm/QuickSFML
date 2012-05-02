4ce7f0baac94c1b04f860dc45fbf7511844997d6
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2012-05-02 20:18:04 +0300

    added Ground Class

---
+++ include/Ground.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Ground.h
+ * Author: mhdsyrwan
+ *
+ * Created on May 2, 2012, 8:17 PM
+ */
+
+#ifndef GROUND_H
+#define	GROUND_H
+
+class Ground {
+public:
+    Ground();
+    Ground(const Ground& orig);
+    virtual ~Ground();
+private:
+
+};
+
+#endif	/* GROUND_H */
+
---
+++ src/Ground.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Ground.cpp
+ * Author: mhdsyrwan
+ * 
+ * Created on May 2, 2012, 8:17 PM
+ */
+
+#include "Ground.h"
+
+Ground::Ground() {
+}
+
+Ground::Ground(const Ground& orig) {
+}
+
+Ground::~Ground() {
+}
+
