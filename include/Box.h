a6607a8453025c6ec88754ba427558cd5c36c84a
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2012-05-02 19:46:40 +0300

    added Box Class

---
+++ include/Box.h
@@ -0,0 +1,31 @@
+/* 
+ * File:   Box.h
+ * Author: mhdsyrwan
+ *
+ * Created on May 1, 2012, 11:33 PM
+ */
+
+#ifndef BOX_H
+#define	BOX_H
+#include "Drawable.h"
+
+class Box : public Drawable{
+public:
+    Box();
+    Box(const Box& orig);
+    
+    void Update(const sf::Input& input);
+    bool LoadGLTextures();
+    void Draw();
+    virtual ~Box();
+private:
+    GLfloat xrot; // X Rotation
+    GLfloat yrot; // Y Rotation
+    GLfloat xspeed; // X Rotation Speed
+    GLfloat yspeed; // Y Rotation Speed
+    GLfloat z; // Depth Into The Screen
+
+};
+
+#endif	/* BOX_H */
+
