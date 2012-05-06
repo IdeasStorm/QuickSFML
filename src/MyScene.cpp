c529117839ae02a175d9d3b6f14646f17979fc65
yaman <m.y.t-91@hotmail.com>
2012-05-06 18:13:51 +0300

    train model

--- src/MyScene.cpp
+++ src/MyScene.cpp
@@ -20,22 +20,23 @@ MyScene::~MyScene() {
 void MyScene::LoadComponents(){
     update_child_controls = true;
     camera.SetOrigin(0,10,0);
-    //Ground *g = new Ground();
-    //g->box_texture = "./Data/Wall/images.jpg";
-    Box *leftWall = new Box(sf::Vector3f (-20,5,-50),sf::Vector3f (1,10,50));
+    Ground *g = new Ground();
+    
+   g->box_texture = "./Data/Wall/images.jpg";
+/*    Box *leftWall = new Box(sf::Vector3f (-20,5,-50),sf::Vector3f (1,10,50));
     Box *Edge = new Box(sf::Vector3f (20,-3,-50),sf::Vector3f (5,1,50));
     Box *ground = new Box(sf::Vector3f (0,-5,-50),sf::Vector3f (20,0.1,50));
     
     leftWall->setTexture("./Data/Wall/wall-texture-high-resolution.jpg");
     Edge->setTexture("./Data/Wall/wall-texture-high-resolution.jpg");
-    
-    Model * model=new Model("monkey.3ds");
-    
-    //components.push_back(g);
-    components.push_back(leftWall);
-    components.push_back(Edge);
-    components.push_back(ground);
-    // components.push_back(model);
+  */ 
+    Model * model=new Model("./AVEENG/AVEENG/AVEENG_L.3DS");
+    //Model * model=new Model("AVEENG_L.3DS");
+    components.push_back(g);
+  //  components.push_back(leftWall);
+//    components.push_back(Edge);
+//    components.push_back(ground);
+     components.push_back(model);
     GLScene::LoadComponents();
 }
 
