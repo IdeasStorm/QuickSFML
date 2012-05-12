12a5095264c6fb3dd15e59c080de42b8155551fa
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2012-05-12 03:43:25 +0300

    added user input header files

---
+++ include/user_input.h
@@ -0,0 +1,21 @@
+//============THIS FILE IS GENERATED FROM USER INPUT================
+#include "gen_init.h" 
+//=====================GENERATED CODE===============================
+void loadUserComponents(list<GLDrawable*>& components) {
+//========================box0=====================================
+Box *box0 = new Box(); 
+box0->setTexture("Data/NeHe.bmp");
+box0->position = Vector3f(0.000000,0.000000,-5.399997); 
+box0->halfSize = Vector3f(1.000000,1.000000,1.000000); 
+box0->setRotation(0.000000,0.000000,0.000000); 
+components.push_back(box0); 
+//==================================================================
+//========================box1=====================================
+Box *box1 = new Box(); 
+box1->setTexture("Data/NeHe.bmp");
+box1->position = Vector3f(0.000000,0.000000,0.000000); 
+box1->halfSize = Vector3f(1.000000,1.000000,1.000000); 
+box1->setRotation(0.000000,0.000000,0.000000); 
+components.push_back(box1); 
+//==================================================================
+}
