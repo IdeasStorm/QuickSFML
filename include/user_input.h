4e2e24d0504f6b459ab96654ee470351fcddb4e4
yaman <m.y.t-91@hotmail.com>
2012-05-13 23:45:09 +0300

    mege light with master

--- include/user_input.h
+++ include/user_input.h
@@ -5,8 +5,8 @@ void loadUserComponents(list<GLDrawable*>& components) {
 //========================box0=====================================
 Box *box0 = new Box();
 box0->setTexture("Data/NeHe.bmp");
-box0->position = Vector3f(4.899998,0.000000,0.000000); 
-box0->halfSize = Vector3f(1.000000,1.000000,1.000000); 
+box0->position = Vector3f(15.200023,3.399999,23.100050); 
+box0->halfSize = Vector3f(3.499999,2.500000,2.900000); 
 box0->setRotation(0.000000,0.000000,0.000000); 
 box0->tag = ""; 
 components.push_back(box0); 
@@ -14,10 +14,11 @@ components.push_back(box0);
 //========================box1=====================================
 Box *box1 = new Box();
 box1->setTexture("Data/NeHe.bmp");
-box1->position = Vector3f(1.900000,-1.300000,11.700008); 
-box1->halfSize = Vector3f(7.899995,5.599997,4.499998); 
+box1->position = Vector3f(6.699996,-4.299998,15.700024); 
+box1->halfSize = Vector3f(25.600061,2.600000,8.499996); 
 box1->setRotation(0.000000,0.000000,0.000000); 
 box1->tag = "building"; 
 components.push_back(box1); 
+//==================================================================
 
 }
