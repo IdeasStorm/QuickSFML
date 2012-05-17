abd694a9d8dedf5a75fc55ce0753a58a95ba7b23
Adnan Khabbaz <adnan.khabbaz@gmail.com>
2012-05-17 21:46:09 +0300

    Added texture to railway

--- src/Railway.cpp
+++ src/Railway.cpp
@@ -26,20 +26,22 @@ Railway::Railway(sf::Vector3f position, sf::Vector3f halfsize) {
     sz=halfsize.z;
             
     left = new Box(sf::Vector3f (x,y,z),sf::Vector3f (sx/10,sy,sz));
+    left->setTexture("./Data/Wall/ironforrail.jpg");
     right = new Box(sf::Vector3f (x+sx,y,z),sf::Vector3f (sx/10,sy,sz));
+    right->setTexture("./Data/Wall/ironforrail.jpg");
      int j=0;
     for(int i=0;i<n;i++)
     {
         Box *temp;
        
         if(i<(n/2))
-            temp=new Box(sf::Vector3f (x+(sx/2),y-sy,z-i),sf::Vector3f (sx,sy,0.05));         
+            temp=new Box(sf::Vector3f (x+(sx/2),y-sy,z-i),sf::Vector3f (sx,sy,0.05));          
         else
         { 
             temp=new Box(sf::Vector3f (x+(sx/2),y-sy,z+i),sf::Vector3f (sx,sy,0.05));
             j++;
         }
-        
+       temp->setTexture("./Data/Wall/woodenForrail.jpg");
        cross.push_back(temp);
     }
 }
