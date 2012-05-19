06acf5fba08e021140eb9ff59f3087ef9fa01ce4
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2012-05-19 15:18:52 +0300

    fixed a a bug

--- include/Train.h
+++ include/Train.h
@@ -32,7 +32,7 @@ public:
     
     void Gas(float acc = 0.1) {
         StopBrakes();
-        push_force = 0.1;
+        push_force = acc;
     }
     
     void Restart(float acc = 0.1){
@@ -40,7 +40,7 @@ public:
         Gas(acc);
     }
     
-    void Brakes(float bf = 0.1f){
+    void Brakes(float bf = 0.05f){
         brakes_factor = bf;
     }
     
