a421ef4c7261dc1ee4d5af6de230d51efac903c3
Adnan Khabbaz <adnan.khabbaz@gmail.com>
2012-05-23 10:09:52 +0300

    Added poster

--- src/Studio.cpp
+++ src/Studio.cpp
@@ -537,6 +537,16 @@ void Studio::ProcessComponent(GLDrawable *component) {
             ((Box*) component)->textures.push_back(tex1);
             ((Box*) component)->textureEnabled = true;
         }
+     else
+        if (tag=="Ad3"){
+            list<Texture> newlist;
+            ((Box*) component)->textures = newlist;
+            Texture tex1("./Data/Textures/poster.jpg");
+            tex1.id = elements::All;
+            ((Box*) component)->tex_per_face = -1;
+            ((Box*) component)->textures.push_back(tex1);
+            ((Box*) component)->textureEnabled = true;
+        }
     
     
 
